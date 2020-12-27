fdfa537ef714a6d52658ecf4c0c01ed6379eb14f
German <pinncho@gmail.com>
2020-12-27 16:54:32 +0600

    Add EncodingFilter

---
+++ src/main/java/com/english/filter/EncodingFilter.java
@@ -0,0 +1,17 @@
+package com.english.filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class EncodingFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        res.setCharacterEncoding(StandardCharsets.UTF_8.toString());
+        chain.doFilter(req, res);
+    }
+}

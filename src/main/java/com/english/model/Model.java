a457906227bee33993fcced7d5fa40d75022bcd7
German <pinncho@gmail.com>
2021-01-04 21:01:34 +0600

    Add base Model class

---
+++ src/main/java/com/english/model/Model.java
@@ -0,0 +1,18 @@
+package com.english.model;
+
+public abstract class Model {
+
+    private long id;
+
+    public Model() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+}

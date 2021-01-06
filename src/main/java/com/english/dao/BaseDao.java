a38b517be9c515b0185c5f2e1aaed94dd88945e1
German <pinncho@gmail.com>
2021-01-06 21:31:44 +0600

    Add BaseDao class

---
+++ src/main/java/com/english/dao/BaseDao.java
@@ -0,0 +1,19 @@
+package com.english.dao;
+
+import com.english.model.Model;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BaseDao<T extends Model> {
+
+    Optional<T> findById(Long id) throws DAOException;
+
+    List<T> findAll();
+
+    void add(T entity);
+
+    void update(T entity);
+
+    void delete(T entity);
+}

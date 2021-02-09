484bd1120f9d4a1325bae76b4ce92b2fe19f6e46
German <pinncho@gmail.com>
2021-02-09 22:57:02 +0600

    Add DAOException constructor with a single String parameter

--- src/main/java/com/english/dao/DAOException.java
+++ src/main/java/com/english/dao/DAOException.java
@@ -4,4 +4,8 @@ public class DAOException extends Exception {
     public DAOException(String message, Throwable e) {
         super(message, e);
     }
+
+    public DAOException(String message) {
+        super(message);
+    }
 }

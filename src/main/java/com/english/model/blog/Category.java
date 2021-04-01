83641ec45ad54a02694b80e31acb2ad91ef4c39f
German <pinncho@gmail.com>
2021-04-01 17:32:28 +0600

    Add Category model

---
+++ src/main/java/com/english/model/blog/Category.java
@@ -0,0 +1,15 @@
+package com.english.model.blog;
+
+import com.english.model.Model;
+
+public class Category extends Model {
+    private String category;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}

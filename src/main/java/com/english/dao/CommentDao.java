7949b2a8d221a1980e07907f0877dc0c7c356a4e
German <pinncho@gmail.com>
2021-04-02 18:14:32 +0600

    Replace incorrect generic class type

--- src/main/java/com/english/dao/CommentDao.java
+++ src/main/java/com/english/dao/CommentDao.java
@@ -1,10 +1,9 @@
 package com.english.dao;
 
-import com.english.model.blog.Category;
 import com.english.model.blog.Comment;
 
 import java.util.List;
 
-public interface CommentDao extends BaseDao<Category> {
-    List<Comment> findByPostId(long postId);
+public interface CommentDao extends BaseDao<Comment> {
+    List<Comment> findCommentsByPostId(long postId);
 }
